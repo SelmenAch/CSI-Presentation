004ac1f011580609400342b8c16bf13174e73fd2
selmenach <selmen.achour@gmail.com>
2020-01-09 23:41:33 +0100

    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PresentationComponent } from './presentation/presentation.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PresentationComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
